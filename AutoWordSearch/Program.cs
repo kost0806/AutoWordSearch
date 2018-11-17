330bef5d6eecc40dc60645653b98be45b2f8d4c9
kost0806 <kimmin0806@gmail.com>
2018-11-17 17:11:46 +0900

    init

---
+++ AutoWordSearch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace AutoWordSearch {
+    static class Program {
+        static Viewer viewer;
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            ClipboardMonitor.Start();
+            ClipboardMonitor.OnClipboardChange += new ClipboardMonitor.OnClipboardChangeEventHandler(ClipboardMonitor_OnClipboardChange);
+            
+            viewer = new Viewer();
+            
+            Application.Run(viewer);
+        }
+
+        static void ClipboardMonitor_OnClipboardChange(ClipboardFormat format, object data) {
+            if (viewer == null)
+                return;
+            if (format.ToString().Equals("Text"))
+                viewer.Search((string)data);
+        }
+    }
+}
---
+++ AutoWordSearch/Viewer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoWordSearch {
+    public partial class Viewer : Form {
+        public Viewer() {
+            InitializeComponent();
+        }
+
+        public void Search(string data) {
+            string word = System.Web.HttpUtility.UrlEncode(data);
+            string url;
+            string[] tmp = data.Split(' ');
+            if (tmp.Length > 1) {
+                url = "https://translate.google.com/#en/ko/" + word;
+            }
+            else {
+                url = "http://dic.naver.com/search.nhn?dicQuery=" + word + "&x=0&y=0&query=" + word + "&target=dic&ie=utf8&query_utf=&isOnlyViewEE=";
+            }
+            webBrowser1.Navigate(url);
+        }
+    }
+}
